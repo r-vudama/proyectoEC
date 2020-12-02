6db5047e5133e784d5b7d0c7b27e63e6df5fe30a
Ramon Urchipia <ramonurchipia@gmail.com>
2020-12-02 08:34:39 -0300

    header

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+      <div className="header-contenedor">
+        <header className="header">
+
+          <h1 className="header__titulo">e-commerce</h1>
+          <nav className="header__nav">
+            <ul className="header__menu">
+                <li><a href="#">PC</a></li>
+                <li><a href="#">SMARTPHONES</a></li>
+                <li><a href="#">SERVICIOS</a></li>
+                <li><a href="#">SOBRE NOSOTROS</a></li>
+            </ul>
+          </nav>
+
+        </header>
+      </div>
+    );
+  }
+
+export default NavBar;
