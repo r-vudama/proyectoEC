59b79795923541b2e8a00a39ee1893e31dc51fa2
Ramon Urchipia <ramonurchipia@gmail.com>
2021-01-03 15:20:25 -0300

    desafio 9

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,29 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const ItemCount = ({stock, initial, onAdd}) => {
+
+    const cantidadRef = useRef()
 
     const [contador, setContador] = useState(initial);
 
     useEffect(() => {
-            if (contador < initial) {return setContador(initial)}
+            if (contador < initial) {setContador(initial)}
             if (contador > stock) {setContador(stock)}
           }, [contador, stock, initial]);
 
     const sumar = () => {
+        onAdd(cantidadRef.current.value)
         setContador(contador + 1)
-        onAdd(contador + 1)
     }
 
     const restar = () => {
+        onAdd(cantidadRef.current.value)
         setContador(contador -1)
-        onAdd(contador -1)
     }
 
     return(
         <div className="contador">
             <div className="botones">
                 <button onClick={restar}>-</button>
-                <input type="number" value={contador} disabled/>
+                <input 
+                    type="number" 
+                    ref={cantidadRef}
+                    value={contador} 
+                    disabled
+                />
                 <button onClick={sumar}>+</button>
             </div>
         </div>
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -5,12 +5,18 @@ import { Link } from 'react-router-dom';
 const ItemDetail = ({title, description, img, valor}) => {
 
     const [cantidad, setCantidad] = useState(1)
+    const [carrito, setCarrito] = useState(null)
 
 
-    const onAdd = (contador) => {
-        setCantidad(contador)
-        console.log('contador ' + contador)
-        console.log('cantidad ' + cantidad)
+    const onAdd = (cantidadRef) => {
+        setCantidad(cantidadRef)
+        // console.log(cantidadRef)
+        // console.log('cantidad ' + cantidad)
+    }
+
+    const addCarrito = () => {
+        setCarrito([title, cantidad])
+        // console.log(carrito)
     }
 
     return(
@@ -20,9 +26,15 @@ const ItemDetail = ({title, description, img, valor}) => {
                 <p className="card-titulo">{title}</p>
                 <p className="card-valor">${valor}</p>
                 <p>{description}</p>
-                <ItemCount stock={10} initial={1} onAdd={onAdd}/>
 
-                <Link to='/cart'><button  className="btn-confirmar">Confirmar Compra</button></Link> 
+                <ItemCount 
+                    stock={10} 
+                    initial={1} 
+                    onAdd={onAdd}
+                />
+
+                {carrito === null ? <button onClick={addCarrito} className='btn-agregar'>Agregar al carrtio</button> : <Link to='/cart'><button  className="btn-confirmar">Confirmar Compra</button></Link> }
+
             </div>
         </div>
     )
